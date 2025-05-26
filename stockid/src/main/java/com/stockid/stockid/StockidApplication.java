62f6697848c89cbd704090e0ea068d222cdb6903
João Felipe Santana de Freitas <j155c137@gmail.com>
2025-05-26 19:32:17 -0300

    first commit - Project creation

---
+++ stockid/src/main/java/com/stockid/stockid/StockidApplication.java
@@ -0,0 +1,13 @@
+package com.stockid.stockid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockidApplication.class, args);
+	}
+
+}
---
+++ stockid/src/test/java/com/stockid/stockid/StockidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockid.stockid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
