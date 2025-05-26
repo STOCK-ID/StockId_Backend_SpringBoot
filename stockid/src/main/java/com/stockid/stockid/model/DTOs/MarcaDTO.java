8c40627cacf2e87c072255e6c534b2957f2d417f
João Felipe Santana de Freitas <j155c137@gmail.com>
2025-05-26 20:39:52 -0300

    Add EqualsAndHashCode annotation to Marca and MarcaDTO classes

--- stockid/src/main/java/com/stockid/stockid/model/DTOs/MarcaDTO.java
+++ stockid/src/main/java/com/stockid/stockid/model/DTOs/MarcaDTO.java
@@ -1,8 +1,10 @@
 package com.stockid.stockid.model.DTOs;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class MarcaDTO extends IntDomainDTO {
 
     private String nome;
--- stockid/src/main/java/com/stockid/stockid/model/Marca.java
+++ stockid/src/main/java/com/stockid/stockid/model/Marca.java
@@ -4,12 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
 public class Marca extends IntDomain {
 
     @Column(unique = false, nullable = false)
