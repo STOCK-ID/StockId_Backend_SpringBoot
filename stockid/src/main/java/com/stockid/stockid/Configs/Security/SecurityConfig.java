44bc61c99a38c75efdf01ab635a1ebca440bec43
João Felipe Santana de Freitas <j155c137@gmail.com>
2025-05-26 22:20:57 -0300

    Add SecurityConfig class to configure security settings for testing

---
+++ stockid/src/main/java/com/stockid/stockid/Configs/Security/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.stockid.stockid.Configs.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeRequests(auth -> auth.anyRequest().permitAll());
+
+        return http.build();
+    }
+}
