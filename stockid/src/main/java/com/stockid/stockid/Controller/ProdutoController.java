536d3c42614d418762b2eb693d133f318bd430ec
João Felipe Santana de Freitas <j155c137@gmail.com>
2025-05-27 21:23:25 -0300

    Add createProduto method to ProdutoService and corresponding endpoint in ProdutoController

--- stockid/src/main/java/com/stockid/stockid/Controller/ProdutoController.java
+++ stockid/src/main/java/com/stockid/stockid/Controller/ProdutoController.java
@@ -6,11 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.stockid.stockid.Service.ProdutoService;
 import com.stockid.stockid.model.Produto;
+import com.stockid.stockid.model.WriteDTOs.ProdutoWriteDTO;
+
+import jakarta.transaction.Transactional;
 
 
 @RestController
@@ -28,5 +33,11 @@ public class ProdutoController {
     @GetMapping("{id}")
     public ResponseEntity<Produto> getProdutoById(@PathVariable Integer id) {
         return ResponseEntity.ok(produtoService.getProdutoById(id));
+    }
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<Produto> createrProduto(@RequestBody ProdutoWriteDTO produtoWriteDTO) {
+        return ResponseEntity.ok(produtoService.createProduto(produtoWriteDTO));
     }
  }
--- stockid/src/main/java/com/stockid/stockid/Service/ProdutoService.java
+++ stockid/src/main/java/com/stockid/stockid/Service/ProdutoService.java
@@ -6,12 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.stockid.stockid.Repository.ProdutoRepository;
+import com.stockid.stockid.model.Marca;
 import com.stockid.stockid.model.Produto;
+import com.stockid.stockid.model.WriteDTOs.ProdutoWriteDTO;
+
+import jakarta.transaction.Transactional;
 @Service
 public class ProdutoService {
 
     @Autowired
     private ProdutoRepository produtoRepository;
+
+    @Autowired
+    private MarcaService marcaService;
 
     public List<Produto> getAllProducts() {
         return produtoRepository.findAll();
@@ -20,5 +27,19 @@ public class ProdutoService {
     public Produto getProdutoById(Integer id) {
         return produtoRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Produto nã encontrado com o id: " + id));
+    }
+
+    @Transactional
+    public Produto createProduto(ProdutoWriteDTO produtoWriteDTO) {
+        Marca marca = marcaService.getMarcaById(produtoWriteDTO.getMarcaId());
+
+        Produto produto = new Produto(marca,
+                produtoWriteDTO.getCategoria(),
+                produtoWriteDTO.getGtin(),
+                produtoWriteDTO.getNome(),
+                produtoWriteDTO.getUnidadeDesc());
+
+        return produtoRepository.save(produto);
+
     }
 }
