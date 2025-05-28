ea4863c5c71f78e47be03be088b0553fafd6884d
João Felipe Santana de Freitas <j155c137@gmail.com>
2025-05-28 00:04:05 -0300

    Change findByGtin method in ProdutoRepository to return Optional<Produto> for better null handling

--- stockid/src/main/java/com/stockid/stockid/Repository/ProdutoRepository.java
+++ stockid/src/main/java/com/stockid/stockid/Repository/ProdutoRepository.java
@@ -1,6 +1,7 @@
 package com.stockid.stockid.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,7 +11,7 @@ import com.stockid.stockid.model.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 
-    public Produto findByGtin(String gtin);
+    public Optional<Produto> findByGtin(String gtin);
 
     public List<Produto> findByCategoria(EnumCategoria categoria);
 
