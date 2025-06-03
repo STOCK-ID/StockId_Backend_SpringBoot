0edb237288b5dd901540b43d0509bb846cc52239
João Felipe Santana de Freitas <j155c137@gmail.com>
2025-06-03 10:15:39 -0300

    Add EstoqueDTO class to represent stock data with product details and status

---
+++ stockid/src/main/java/com/stockid/stockid/model/DTOs/EstoqueDTO.java
@@ -0,0 +1,30 @@
+package com.stockid.stockid.model.DTOs;
+
+import java.time.LocalDate;
+
+import com.stockid.stockid.enums.EnumStatusLote;
+import com.stockid.stockid.model.Produto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+@AllArgsConstructor
+public class EstoqueDTO extends IntDomainDTO {
+
+    private Produto produto;
+    private String lote;
+    private Integer quantidadeAtual;
+    private Integer quantidadeMinima;
+    private String unidadeMedida;
+    private LocalDate validade;
+    private LocalDate entrada;
+    private Double precoCompra;
+    private Double precoVenda;
+    private EnumStatusLote statusLote;
+
+}
