df88d94411f9007641f3e9a98ae003b6475d63e2
João Felipe Santana de Freitas <j155c137@gmail.com>
2025-06-10 08:39:22 -0300

    Add EstoqueController and EstoqueService for managing stock creation

---
+++ stockid/src/main/java/com/stockid/stockid/Controller/EstoqueController.java
@@ -0,0 +1,28 @@
+package com.stockid.stockid.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.stockid.stockid.Service.EstoqueService;
+import com.stockid.stockid.model.WriteDTOs.EstoqueWriteDTO;
+
+@RestController
+@RequestMapping("api/estoque")
+public class EstoqueController {
+
+    @Autowired
+    EstoqueService estoqueService;
+
+    @PostMapping
+    public ResponseEntity createLote(@RequestBody EstoqueWriteDTO estoqueWriteDTO) {
+        try {
+            return ResponseEntity.ok().body(estoqueService.createLote(estoqueWriteDTO));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
---
+++ stockid/src/main/java/com/stockid/stockid/Service/EstoqueService.java
@@ -0,0 +1,40 @@
+package com.stockid.stockid.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.stockid.stockid.Repository.EstoqueRepository;
+import com.stockid.stockid.model.Estoque;
+import com.stockid.stockid.model.Produto;
+import com.stockid.stockid.model.WriteDTOs.EstoqueWriteDTO;
+
+@Service
+public class EstoqueService {
+
+    @Autowired
+    EstoqueRepository estoqueRepository;
+
+    @Autowired
+    ProdutoService produtoService;
+
+    public Estoque createLote(EstoqueWriteDTO estoqueWriteDTO) {
+        Produto produto = produtoService.getProdutoById(estoqueWriteDTO.getProdutoId());
+        
+        Estoque newEstoque = new Estoque(
+            produto,
+            estoqueWriteDTO.getLote(),
+            estoqueWriteDTO.getQuantidadeAtual(),
+            estoqueWriteDTO.getQuantidadeMinima(),
+            estoqueWriteDTO.getUnidadeMEdida(),
+            estoqueWriteDTO.getValidade(),
+            estoqueWriteDTO.getEntrada(),
+            estoqueWriteDTO.getPrecoCompra(),
+            estoqueWriteDTO.getPrecoVenda(),
+            estoqueWriteDTO.getStatusLote()
+        );
+
+        Estoque estoque = estoqueRepository.save(newEstoque);
+
+        return estoque;
+    }
+}
