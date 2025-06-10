21c80c05c4b30962ae65fef66583ad1292a06df2
João Felipe Santana de Freitas <j155c137@gmail.com>
2025-06-10 08:41:03 -0300

    Refactor EstoqueWriteDTO to use LocalDate for validade and entrada fields

--- stockid/src/main/java/com/stockid/stockid/model/WriteDTOs/EstoqueWriteDTO.java
+++ stockid/src/main/java/com/stockid/stockid/model/WriteDTOs/EstoqueWriteDTO.java
@@ -1,5 +1,7 @@
 package com.stockid.stockid.model.WriteDTOs;
 
+import java.time.LocalDate;
+
 import com.stockid.stockid.enums.EnumStatusLote;
 
 import lombok.Data;
@@ -11,8 +13,8 @@ public class EstoqueWriteDTO {
     private Integer quantidadeAtual;
     private Integer quantidadeMinima;
     private String unidadeMEdida;
-    private String validade;
-    private String entrada;
+    private LocalDate validade;
+    private LocalDate entrada;
     private Double precoCompra;
     private Double precoVenda;
     private EnumStatusLote statusLote;
