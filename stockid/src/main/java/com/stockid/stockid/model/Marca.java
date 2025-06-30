f4accdcbbaf41ce5cb5b2cd39548157200e01917
João Felipe Santana de Freitas <j155c137@gmail.com>
2025-06-30 19:34:20 -0300

    Refactor Marca class to implement DTOConvertable interface and add getDTOClass method

--- stockid/src/main/java/com/stockid/stockid/model/Marca.java
+++ stockid/src/main/java/com/stockid/stockid/model/Marca.java
@@ -1,5 +1,9 @@
 package com.stockid.stockid.model;
 
+import com.spec.speedspring.core.dtoConvertable.DTOConvertable;
+import com.stockid.stockid.model.DTOs.MarcaDTO;
+import com.stockid.stockid.model.WriteDTOs.MarcaWriteDTO;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
@@ -12,8 +16,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = false)
-public class Marca extends IntDomain {
+public class Marca extends IntDomain implements DTOConvertable<MarcaWriteDTO, MarcaDTO>{
 
     @Column(unique = false, nullable = false)
     private String nome;
+
+    @Override
+    public Class<MarcaDTO> getDTOClass() {
+        return MarcaDTO.class;
+    }
+
+    public boolean getLog() {
+        return false;
+    }
+
+
 }
