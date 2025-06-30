fb368fccc3a7ec21285be686a775d04ae4b4d904
João Felipe Santana de Freitas <j155c137@gmail.com>
2025-06-30 11:27:38 -0300

    Add terminal clearing functionality in StockidApplication

--- stockid/src/main/java/com/stockid/stockid/StockidApplication.java
+++ stockid/src/main/java/com/stockid/stockid/StockidApplication.java
@@ -3,10 +3,25 @@ package com.stockid.stockid;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.stockid.stockid.Service.AnsiColorUtil;
+import com.stockid.stockid.Service.ComunService;
+
 @SpringBootApplication
 public class StockidApplication {
-
 	public static void main(String[] args) {
+		
+		ComunService.clearTerminal();
+
+		System.out.println(AnsiColorUtil.colorText(
+						" ######  ########  #######   ######  ##    ##    #### ########  \n" +
+						"##    ##    ##    ##     ## ##    ## ##   ##      ##  ##     ## \n" +
+						"##          ##    ##     ## ##       ##  ##       ##  ##     ## \n" +
+						" ######     ##    ##     ## ##       #####        ##  ##     ## \n" +
+						"      ##    ##    ##     ## ##       ##  ##       ##  ##     ## \n" +
+						"##    ##    ##    ##     ## ##    ## ##   ##      ##  ##     ## \n" +
+						" ######     ##     #######   ######  ##    ##    #### ########  ",
+				"PURPLE"));
+	
 		SpringApplication.run(StockidApplication.class, args);
 	}
 
